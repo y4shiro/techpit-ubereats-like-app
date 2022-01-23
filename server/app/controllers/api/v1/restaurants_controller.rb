15f39fead912f21e6aed8688fb5227f6fb2dae6e
y4shiro <y4shironao@gmail.com>
2022-01-23 18:39:56 +0900

    Restaurant コントローラーを作成し、店舗一覧を返す index メソッドを実装

---
+++ server/app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class RestaurantsController < ApplicationController
+      def index
+        restaurants = Restaurant.all
+
+        render json: { restaurants: restaurants }, status: :ok
+      end
+    end
+  end
+end
