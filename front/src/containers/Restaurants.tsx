0e65e33f87f326facac2a80da66d822dda605d0e
y4shiro <y4shironao@gmail.com>
2022-01-24 18:44:53 +0900

    フロント側からレストラン一覧の API を叩いてデータを取得

--- front/src/containers/Restaurants.tsx
+++ front/src/containers/Restaurants.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { fetchRestaurants } from '../apis/restaurants';
 
 export const Restaurants: React.VFC = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => console.log(data));
+  }, []);
+
   return <>レストラン一覧</>;
 };
