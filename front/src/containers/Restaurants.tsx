847d2e04a801da24e7e51dec3c99643c77cac4cf
y4shiro <y4shironao@gmail.com>
2022-01-25 10:44:19 +0900

    Restaurant コンポーネントのビュー実装

--- front/src/containers/Restaurants.tsx
+++ front/src/containers/Restaurants.tsx
@@ -1,10 +1,42 @@
 import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
 import { fetchRestaurants } from '../apis/restaurants';
+
+import MainLogo from '../images/logo.png';
+import MainCoverImage from '../images/main-cover-image.png';
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants: React.VFC = () => {
   useEffect(() => {
     fetchRestaurants().then((data) => console.log(data));
   }, []);
 
-  return <>レストラン一覧</>;
+  return (
+    <>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </>
+  );
 };
